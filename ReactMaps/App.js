f9d8f21a05b27b0e061c2dd377fb4a41a7a26582
Karlos Kelvin <karloskelvinsantos@gmail.com>
2017-11-09 19:09:03 -0300

    Finalização do App ReactMaps

--- ReactMaps/App.js
+++ ReactMaps/App.js
@@ -9,52 +9,106 @@ import {
 
 import MapView from 'react-native-maps';
 
+const { height, width } = Dimensions.get( 'window' );
+
 export default class App extends Component {
 
   state = {
-    latitude: -7.213623,
-    longitude: -39.328866,
+    places: [
+      {
+        id: 1,
+        title: 'Igreja do Salesianos',
+        description: 'Igreja católica do Salesianos',
+        latitude: -7.210608,
+        longitude: -39.321847, 
+      },
+      {
+        id: 2,
+        title: 'Cariri Garden',
+        description: 'Shopping Center',
+        latitude: -7.223421,
+        longitude: -39.324573,
+      },
+      {
+        id: 3,
+        title: 'Santuário São Francisco das Chagas',
+        description: 'Igreja católica dos Franciscanos',
+        latitude: -7.211822,
+        longitude: -39.313908,
+      }
+    ],
+  };
+
+  _mapReady = () => {
+    this.state.places[0].mark.showCallout();
   }
 
   render() {
-    const { latitude, longitude } = this.state;
+    const { latitude, longitude } = this.state.places[0];
 
     return (
       <View style={styles.container}>
         <MapView
+          ref={map => this.mapview = map}
           initialRegion={{
             latitude,
             longitude,
-            latitudeDelta: 0.0042,
-            longitudeDelta: 0.0031,
+            latitudeDelta: 0.0142,
+            longitudeDelta: 0.0231,
           }}
           style={styles.mapView}
-          rotateEnabled={false}
-          scrollEnabled={false}
-          zoomEnabled={false}
           showsBuildings={false}
           showsPointsOfInterest={false}
+          onMapReady={this._mapReady}
         >
-          <MapView.Marker
-            coordinate={{
-              latitude: -7.213623,
-              longitude: -39.328866,
-            }}
-          />
+         {this.state.places.map(place => (
+            <MapView.Marker
+            ref={mark => place.mark = mark}
+              key={place.id}
+              title={place.title}
+              description={place.description}
+              coordinate={{
+                latitude: place.latitude,
+                longitude: place.longitude,
+              }}
+            />
+         ))}
         </MapView>
         <ScrollView 
           style={styles.placesContainer}
           horizontal
+          showsHorizontalScrollIndicator={false}
+          pagingEnabled
+          onMomentumScrollEnd={e => {
+            const scrolled = e.nativeEvent.contentOffset.x;
+
+            const place = (scrolled > 0)
+              ? scrolled / Dimensions.get('window').width
+              : 0;
+
+              const { latitude, longitude, mark} = this.state.places[place];
+
+              this.mapview.animateToCoordinate({
+                latitude,
+                longitude,
+              }, 1000);
+
+              setTimeout(() => {
+                mark.showCallout();
+              }, 1000);
+          }}
           >
-            <View style={styles.place}></View>
-            <View style={styles.place}></View>
+          {this.state.places.map(place => (
+            <View key={place.id} style={styles.place}>
+              <Text>{place.title}</Text>
+              <Text>{place.description}</Text>
+            </View>
+          ))}
         </ScrollView>
       </View>
     );
   }
 }
-
-const { height, width } = Dimensions.get( 'window' );
 
 const styles = StyleSheet.create({
   container: {
