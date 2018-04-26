e4bf48f1319fefff4b69f8c15466279d598c83ad
Karlos Kelvin <kelvin.santos@ufca.edu.br>
2018-04-26 12:25:01 -0300

    Consumindo e estilizando Orgs

--- CursoGoNative/GoNative-Mod02/src/pages/organizations/index.js
+++ CursoGoNative/GoNative-Mod02/src/pages/organizations/index.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import api from 'services/api';
+import {
+  View,
+  Text,
+  AsyncStorage,
+  ActivityIndicator,
+  FlatList,
+} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+
+import styles from './styles';
+import OrganizationItem from './components/OrganizationItem';
 
 export default class Organizations extends Component {
   static navigationOptions = {
@@ -8,7 +18,43 @@ export default class Organizations extends Component {
     tabBarIcon: ({ tintColor }) => <Icon name="building" size={24} color={tintColor} />,
   };
 
+  state = {
+    data: [],
+    loading: true,
+  };
+
+  componentDidMount() {
+    this.loadOrganizations();
+  }
+
+  loadOrganizations = async () => {
+    const username = await AsyncStorage.getItem('@Githuber:username');
+    const response = await api.get(`/users/${username}/orgs`);
+
+    this.setState({ data: response.data, loading: false });
+  };
+
+  renderListItem = ({ item }) => (
+    <OrganizationItem organization={item} />
+  )
+
+  renderList = () => (
+    <FlatList
+      data={this.state.data}
+      keyExtractor={item => String(item.id)}
+      renderItem={this.renderListItem}
+      numColumns={2}
+      columnWrapperStyle={styles.columnContainer}
+    />
+  );
+
   render() {
-    return <View />;
+    return (
+      <View style={styles.container} >
+        { this.state.loading
+          ? <ActivityIndicator style={styles.loading} />
+          : this.renderList() }
+      </View>
+    );
   }
 }
