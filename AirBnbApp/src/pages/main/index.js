14995350f570d3e3311bb8d4f79117b0c3843db1
Karlos Kelvin <karloskelvinsantos@gmail.com>
2018-08-06 23:02:08 -0300

    Adicionado Mapa na Tela Principal

--- AirBnbApp/src/pages/main/index.js
+++ AirBnbApp/src/pages/main/index.js
@@ -1,9 +1,65 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import { View } from 'react-native';
+import { StatusBar } from 'react-native';
 
-const Main = () => (
-    <View />
-);
+import MapboxGL from '@mapbox/react-native-mapbox-gl';
+import { Container, AnnotationContainer, AnnotationText } from './styles';
 
-export default Main;
+import api from '../../services/api';
+
+MapboxGL.setAccessToken('pk.eyJ1Ijoia2FybG9za2VsdmluIiwiYSI6ImNqaXc3ZWw3NjF1NTAzcm8zdTN0M2Fqb3oifQ.XS64D6yjdmJRl1rTnHplPg');
+
+export default class Main extends Component {
+
+  static navigationOptions = {
+    header: null,
+  };
+
+  state = {
+    locations: [],
+  };
+
+  async componentDidMount() {
+    try {
+      const response = await api.get('/properties', {
+        params: {
+          latitude: -7.213801, 
+          longitude: -39.328920,
+        },
+      });
+
+      this.setState({ locations: response.data });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  renderLocations = () => (
+    this.state.locations.map(location => (
+      <MapboxGL.PointAnnotation
+        id={location.id.toString()}
+        coordinate={[parseFloat(location.longitude), parseFloat(location.latitude)]}
+      >
+        <AnnotationContainer>
+          <AnnotationText>{location.price}</AnnotationText>
+        </AnnotationContainer>
+        <MapboxGL.Callout title={locaton.title}/>
+      </MapboxGL.PointAnnotation>
+    ))
+  );
+  
+  render() {
+    return (
+      <Container>
+        <StatusBar barStyle="light-content"/>
+        <MapboxGL.MapView
+          centerCoordinate={[-7.213801, -39.328920]}
+          style={{ flex: 1}}
+          styleURL={MapboxGL.StyleURL.Dark}
+        >
+         { this.renderLocations() }
+        </MapboxGL.MapView>
+      </Container>
+    )
+  }
+}
--- AirBnbApp/src/services/api.js
+++ AirBnbApp/src/services/api.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { AsyncStorage } from 'react-native';
 
 /* Endereços para cada emulador/simulador:
 ** Genymotion:              http://10.0.3.2:3333/
@@ -7,7 +8,21 @@ import axios from 'axios';
 */
 
 const api = axios.create({
-    baseURL: 'http://10.0.3.2:3333/',
+    baseURL: 'https://airbnb-server.herokuapp.com/',
+});
+
+api.interceptors.request.use(async (config) => {
+  try {
+    const token = await AsyncStorage.getItem('@AirBnbApp:token');
+
+    if (token) {
+      config.headers.Authorization = `Baerer ${token}`;
+    }
+
+    return config;
+  } catch (error) {
+    alert(error);
+  }
 });
 
 export default api;
