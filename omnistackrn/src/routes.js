2e9e2df907a1c0b69811ef3debf597530c2d1a21
Kelvin Santos <kelvin.santos@ufca.edu.br>
2019-02-07 17:28:59 -0300

    GoWeek - AppRn - Twitter

---
+++ omnistackrn/src/routes.js
@@ -0,0 +1,17 @@
+import { createAppContainer, createSwitchNavigator, createStackNavigator } from 'react-navigation';
+
+import Login from './pages/Login';
+import Timeline from './pages/Timeline';
+import NewTweet from './pages/NewTweet';
+
+const Routes = createAppContainer(
+  createSwitchNavigator({
+    Login,
+    App: createStackNavigator({
+      Timeline,
+      NewTweet,
+    })
+  })
+);
+
+export default Routes;
